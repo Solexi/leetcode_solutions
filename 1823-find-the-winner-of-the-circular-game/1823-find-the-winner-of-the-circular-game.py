9460cbc3e4fd8a27fe76a95582c2c89900e4f242
Sophia Ezeh <82169299+Solexi@users.noreply.github.com>
2024-02-29 19:29:40 +0100

    Time: 44 ms (46.42%), Space: 16.7 MB (59.57%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -4,7 +4,6 @@ class Solution:
         index = 0
         for i in range(1, n+1):
             arr.append(i)
-            # arr = [i for i in range(1, n+1)]
         while len(arr) > 1:
             index = (index + k - 1) % len(arr)
             arr.pop(index)
